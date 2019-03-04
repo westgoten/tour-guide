8dadcd69d90d5f5d89b4d5fc9f4bf4818b66dac3
westgoten <rodrigodeoliveira397@gmail.com>
2019-03-04 19:21:00 -0300

    Set up swipe view feature: - Created AttractionsPagerAdapter; - Created AttractionListFragment and its layout.

---
+++ app/src/main/java/com/example/rodrigo/tourguide/AttractionListFragment.java
@@ -0,0 +1,39 @@
+package com.example.rodrigo.tourguide;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AttractionListFragment extends Fragment {
+    private int position;
+
+    public static final String ARG_POSITION = "position";
+    private static final String TAG = "AttractionListFragment";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        position = (args != null) ? args.getInt(ARG_POSITION) : 0;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ImageView imageView = (ImageView) inflater.inflate(R.layout.fragment_attraction_list, container, false);
+
+        if (position == 0) {
+            imageView.setImageResource(R.drawable.before);
+        } else {
+            imageView.setImageResource(R.drawable.after);
+        }
+
+        return imageView;
+    }
+}
--- app/src/main/java/com/example/rodrigo/tourguide/MainActivity.java
+++ app/src/main/java/com/example/rodrigo/tourguide/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.rodrigo.tourguide;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NUM_OF_TABS = 2;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AttractionsViewPagerAdapter pagerAdapter = new AttractionsViewPagerAdapter(getSupportFragmentManager());
+
+        ViewPager viewPager = findViewById(R.id.pager);
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    private class AttractionsViewPagerAdapter extends FragmentPagerAdapter {
+        public AttractionsViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = new AttractionListFragment();
+            Bundle args = new Bundle();
+            args.putInt(AttractionListFragment.ARG_POSITION, position);
+            fragment.setArguments(args);
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_OF_TABS;
+        }
     }
 }
