a9821dae6290e0417e7f00cf6e9bd7c4b31fcf2d
westgoten <rodrigodeoliveira397@gmail.com>
2019-03-04 23:05:30 -0300

    Changed native ActionBar for a Toolbar; Set up TabLayout integrated with ViewPager.

--- app/src/main/java/com/example/rodrigo/tourguide/AttractionListFragment.java
+++ app/src/main/java/com/example/rodrigo/tourguide/AttractionListFragment.java
@@ -1,7 +1,6 @@
 package com.example.rodrigo.tourguide;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/example/rodrigo/tourguide/MainActivity.java
+++ app/src/main/java/com/example/rodrigo/tourguide/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.rodrigo.tourguide;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int NUM_OF_TABS = 2;
+    private static final int NUM_OF_TABS = 4;
     private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null)
+                actionBar.hide();
+        }
 
         AttractionsViewPagerAdapter pagerAdapter = new AttractionsViewPagerAdapter(getSupportFragmentManager());
 
@@ -27,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             super(fm);
         }
 
+        @NonNull
         @Override
         public Fragment getItem(int position) {
             Fragment fragment = new AttractionListFragment();
@@ -40,6 +55,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return NUM_OF_TABS;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "tab " + position;
         }
     }
 }
